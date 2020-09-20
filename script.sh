7f2c6fd99c5d5677ff6d6ee5658576b57a179931
Gabriel Meghnagi <gabrielmeghnagi@outlook.it>
2020-09-20 15:36:07 +0000

    Modified script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
-set -xu
+set -exu
+# -e : Exit immediately if a command exits with a non-zero status
+# -x : Print commands and their arguments as they are executed
+# -u : Treat unset variables as an error when substituting
 python3 redbot.py
+exit ${PIPESTATUS[0]}
