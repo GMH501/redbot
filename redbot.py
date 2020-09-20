e9bab6ca829e499c5ebe8e52feb5d32a05a0a637
root <root@docker01.example.local>
2020-09-20 14:32:51 +0000

    Added redbot.py

--- redbot.py
+++ redbot.py
@@ -7,5 +7,4 @@ redmine_url = os.getenv('REDMINE_URL')
 user = os.getenv('REDMINE_USER')
 pwd = os.getenv('REDMINE_PASSWORD')
 
-app = Flask(__name__)
 redmine = Redmine(redmine_url, username=user, password=pwd)
