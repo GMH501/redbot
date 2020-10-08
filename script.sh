512ffe1facf13bbd0d801e1c729ead93ab3b3614
GMH501 <40721844+GMH501@users.noreply.github.com>
2020-10-08 18:08:07 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,7 @@ set -exu
 # -e : Exit immediately if a command exits with a non-zero status
 # -x : Print commands and their arguments as they are executed
 # -u : Treat unset variables as an error when substituting
-python3 /redbot/redbot.py
+while true; do
+ls
+sleep 300
+done
